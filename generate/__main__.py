62e487db63278d61a60d94885e5b2fefeb17da62
Sam Larsen <sam@slarsen.io>
2025-02-12 01:44:15 -0500

    Add feature arguments

--- generate/__main__.py
+++ generate/__main__.py
@@ -8,55 +8,109 @@ parser = argparse.ArgumentParser(
 )
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument(
-    "--polygrams", action="store_true", help="plot regular star polygons"
-)
-group.add_argument("--spirals", action="store_true", help="plot spirals")
-parser.add_argument(
-    "n",
+    "--polygrams",
     type=int,
-    nargs="?",
-    default=30,
-    help="number for total configurations",
+    metavar="N",
+    help="plot multiple regular star polygons with N configurations",
+)
+group.add_argument(
+    "--spirals",
+    type=int,
+    metavar="N",
+    help="plot multiple spirals with N configurations",
+)
+group.add_argument(
+    "--polygram",
+    type=int,
+    metavar="N",
+    help="plot a single star polygon with N vertices",
+)
+group.add_argument(
+    "--spiral", type=int, metavar="N", help="plot a single spiral with N vertices"
+)
+parser.add_argument(
+    "--save",
+    action="store_true",
+    help="save the diagram to the renders folder",
 )
 args = parser.parse_args()
 
-if args.n <= 0:
-    parser.error("number must be greater than 0")
+if args.polygrams and args.polygrams < 6:
+    parser.error("number must be 6 or greater")
+if args.spirals and args.spirals < 6:
+    parser.error("number must be 6 or greater")
+if args.polygram and args.polygram < 5:
+    parser.error("number must be 5 or greater")
+if args.spiral and args.spiral < 5:
+    parser.error("number must be 5 or greater")
 
-length = args.n + 4
+if args.polygrams:
+    length = args.polygrams + 4
+elif args.spirals:
+    length = args.spirals + 4
 
 polygrams = args.polygrams
 spirals = args.spirals
+polygram = args.polygram
+spiral = args.spiral
 
-configs = []
-for n in range(5, length + 1):
-    step = 2
-    configs.append((n, step))
+if polygrams or spirals:
+    configs = []
+    for n in range(5, length + 1):
+        step = 2
+        configs.append((n, step))
 
-num_plots = len(configs)
-rows = (num_plots + 5) // 6
-fig = plt.figure(figsize=(18, 3 * rows), dpi=300)
+    num_plots = len(configs)
+    rows = (num_plots + 5) // 6
+    dpi = 300 if args.save else 60
+    fig = plt.figure(figsize=(18, 3 * rows), dpi=dpi)
 
-for i, (n, step) in enumerate(configs):
-    ax = fig.add_subplot(rows, 6, i + 1)
+    for i, (n, step) in enumerate(configs):
+        ax = fig.add_subplot(rows, 6, i + 1)
+        if polygrams:
+            plot_star_polygon(n, step, ax)
+        elif spirals:
+            plot_logarithmic_spiral(n, step, ax)
+        else:
+            parser.print_help()
+            parser.exit(1)
+
     if polygrams:
-        plot_star_polygon(n, step, ax)
+        name = "Regular Star Polygrams"
+        file = "regular_star_polygons.png"
     elif spirals:
-        plot_logarithmic_spiral(n, step, ax)
+        name = "Logarithmic Spirals"
+        file = "logarithmic_spirals.png"
+
+    plt.tight_layout()
+
+    if args.save:
+        timestamp = int(time())
+        plt.savefig(f"generate/renders/{timestamp}_{file}", dpi=300)
+        plt.close()
+
+        print(f"\n✨ {name} diagram saved to generate/renders/{timestamp}_{file} 💫\n")
     else:
-        parser.print_help()
-        parser.exit(1)
+        plt.gcf().canvas.manager.set_window_title(f"{name}")
+        plt.show()
 
-if polygrams:
-    name = "Polygrams"
-    file = "regular_star_polygons.png"
-elif spirals:
-    name = "Spirals"
-    file = "logarithmic_spirals.png"
+elif polygram or spiral:
+    if polygram:
+        plot_star_polygon(polygram, 2, plt.gca())
+        name = "Regular Star Polygon"
+        file = "regular_star_polygon.png"
+    elif spiral:
+        plot_logarithmic_spiral(spiral, 2, plt.gca())
+        name = "Logarithmic Spiral"
+        file = "logarithmic_spiral.png"
 
-timestamp = int(time())
-plt.tight_layout()
-plt.savefig(f"generate/renders/{timestamp}_{file}", dpi=300)
-plt.close()
+    plt.tight_layout()
 
-print(f"\n✨ {name} diagram saved to generate/renders/{file} 💫\n")
+    if args.save:
+        timestamp = int(time())
+        plt.savefig(f"generate/renders/{timestamp}_{file}", dpi=300)
+        plt.close()
+        print(f"\n✨ {name} diagram saved to generate/renders/{timestamp}_{file} 💫\n")
+    else:
+        plt.gcf().canvas.manager.set_window_title(f"{name}")
+        plt.show()
